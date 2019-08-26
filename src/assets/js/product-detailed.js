2ad1d2ba4cddc6910dbc9551ad977e9dcc1d863c
Lucas Bertoldo <lucas.fber@gmail.com>
2019-08-26 08:25:24 -0300

    button scroll done

--- src/assets/js/product-detailed.js
+++ src/assets/js/product-detailed.js
@@ -1,2 +1,13 @@
 /* https://slickmedia.co.uk/blog/glenns-blog/limit-number-of-characters-in-div-jquery-css/ */
 /* https://www.w3schools.com/howto/howto_js_scroll_to_top.asp */
+import $ from '../../../node_modules/jquery/dist/jquery.slim';
+
+const $buttonScroll = $('.button-scroll');
+
+$buttonScroll.click(function() {
+  scrollToTop();
+});
+
+const scrollToTop = () => {
+  document.documentElement.scrollTop = 0;
+};
--- src/index.js
+++ src/index.js
@@ -1,58 +1,59 @@
-import "./assets/scss/style.scss";
-import $ from "../node_modules/jquery/dist/jquery";
-import "./assets/js/carousel";
-import "./assets/js/carousel-products";
-import "./assets/js/action-bar";
-import "./assets/js/product-tile-grid";
-import "./assets/js/feature-products-slider";
-import "./assets/js/category-landing";
+import './assets/scss/style.scss';
+import $ from '../node_modules/jquery/dist/jquery';
+import './assets/js/carousel';
+import './assets/js/carousel-products';
+import './assets/js/action-bar';
+import './assets/js/product-tile-grid';
+import './assets/js/feature-products-slider';
+import './assets/js/category-landing';
+import './assets/js/product-detailed';
 
 $(document).ready(function() {
-  const dropdown = $(".navbar ul li");
-  const headerMain = $(".header-main");
-  const btnMenu = $(".btn-close");
-  const btnClose = $(".btn-menu");
-  const navbar = $(".navbar");
-  const footerHeader = $("footer h4");
-  const navbarLink = $(".navbar ul li a");
-  const menuItem = $(".menu__menu-item h4");
-  let togglerPassword = $("#toggler");
-  let inputPassword = $("#password");
-  let userIcon = $(".flaticon-man");
-  let modalLogin = $(".modal-login");
-  let modalLoginForm = $(".modal-login__form"); // is this variable really necessary?
+  const dropdown = $('.navbar ul li');
+  const headerMain = $('.header-main');
+  const btnMenu = $('.btn-close');
+  const btnClose = $('.btn-menu');
+  const navbar = $('.navbar');
+  const footerHeader = $('footer h4');
+  const navbarLink = $('.navbar ul li a');
+  const menuItem = $('.menu__menu-item h4');
+  let togglerPassword = $('#toggler');
+  let inputPassword = $('#password');
+  let userIcon = $('.flaticon-man');
+  let modalLogin = $('.modal-login');
+  let modalLoginForm = $('.modal-login__form'); // is this variable really necessary?
 
   menuItem.click(function(e) {
     e.stopPropagation();
     $(this)
       .parent()
-      .children(".menu__submenu")
+      .children('.menu__submenu')
       .toggle();
   });
 
   footerHeader.click(function() {
     $(this)
       .parent()
-      .children(".wrapper")
+      .children('.wrapper')
       .toggle();
 
     $(this)
-      .children("span.icon")
-      .children("i")
-      .toggleClass("flaticon-caret-down")
-      .toggleClass("flaticon-arrow");
+      .children('span.icon')
+      .children('i')
+      .toggleClass('flaticon-caret-down')
+      .toggleClass('flaticon-arrow');
   });
 
   navbarLink.click(function(e) {
     //e.preventDefault();
     $(this)
       .parent()
-      .children(".menu")
+      .children('.menu')
       .toggle();
   });
 
   function setCurrentYear() {
-    let currentYear = $("#current-year");
+    let currentYear = $('#current-year');
     currentYear.text(new Date().getFullYear());
   }
 
@@ -63,29 +64,29 @@ $(document).ready(function() {
   }
 
   function toggleDropdown(e) {
-    $(".dropdown").hide();
+    $('.dropdown').hide();
     e.stopPropagation();
     $(e.currentTarget)
-      .children(".dropdown")
+      .children('.dropdown')
       .toggle();
   }
 
   function showPassword() {
-    inputPassword.attr("type", "text");
+    inputPassword.attr('type', 'text');
   }
 
   function hidePassword() {
-    inputPassword.attr("type", "password");
+    inputPassword.attr('type', 'password');
   }
 
   function showModal() {
-    headerMain.css("z-index", 0);
-    modalLogin.css("display", "flex");
+    headerMain.css('z-index', 0);
+    modalLogin.css('display', 'flex');
   }
 
   function handleClickOutsideModal() {
     $(window).click(function() {
-      $(".dropdown").hide();
+      $('.dropdown').hide();
       // console.log(headerMain.css('z-index'));
       /** AO MOSTRAR O MENU DA NAVBAR SETAR O Z-INDEX DO MAIN-HEADER DE VOLTA PRA 2 */
     });
@@ -103,8 +104,8 @@ $(document).ready(function() {
 
   modalLogin.click(function(e) {
     if (e.target === this) {
-      modalLogin.css("display", "none");
-      modalLoginForm.trigger("reset");
+      modalLogin.css('display', 'none');
+      modalLoginForm.trigger('reset');
     }
   });
 
