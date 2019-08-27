9c25ddbd55c227b01d743821bba272be964a0515
Lucas Bertoldo <lucas.fber@gmail.com>
2019-08-27 16:20:04 -0300

    carousel, popular items tablet

--- src/assets/js/product-tile-grid.js
+++ src/assets/js/product-tile-grid.js
@@ -1,7 +1,7 @@
-import $ from "../../../node_modules/jquery/dist/jquery.min";
-import { addToShoppingBag, addToWishlist } from "./action-bar";
-const $buttonLoadMore = $("#button-load-more");
-const $productTileGrid = $(".products-grid");
+import $ from '../../../node_modules/jquery/dist/jquery.min';
+import { addToShoppingBag, addToWishlist } from './action-bar';
+const $buttonLoadMore = $('#button-load-more');
+const $productTileGrid = $('.products-grid');
 
 /**
  * @constant
@@ -9,21 +9,19 @@ const $productTileGrid = $(".products-grid");
  * An URI to a simple server which give us the json for products.
  */
 const URI =
-  "https://my-json-server.typicode.com/lucasfber/osf-database/products";
-
-$buttonLoadMore.click(function() {
-  getData();
-});
+  'https://my-json-server.typicode.com/lucasfber/osf-database/products';
 
 /**
  * Make AJAX request to server and get the product data (A JSON Array).
  *
  */
 function getData() {
+  const viewportWidth = $(window).width();
+  let quantityProductTiles = viewportWidth < 1280 ? 2 : 4;
+
   $.get(URI, function(data, status) {
     shuffle(data);
-    console.log(data);
-    createGridRow(data.slice(0, 4));
+    createGridRow(data.slice(0, quantityProductTiles));
   });
 }
 
@@ -48,43 +46,43 @@ export function createProductTile(product) {
  * @returns A node element to be append on the DOM
  */
 function createDefaultProductTile(product) {
-  let productTile = $("<div></div>");
+  let productTile = $('<div></div>');
 
-  let productTileImage = $("<div></div>");
+  let productTileImage = $('<div></div>');
   let productImage = $(`<img src=${product.imageUrl} />`);
-  productImage.attr("alt", product.name);
+  productImage.attr('alt', product.name);
 
-  let productTileInfo = $("<div></div>");
+  let productTileInfo = $('<div></div>');
   let productName = $(`<p>${product.name}</p>`);
   let productPrice = $(`<span>$ ${product.price}</span>`);
 
-  let overlay = $("<div></div>");
-  let overlayButtonBox = $("<div></div>");
-  let buttonPlus = $("<div></div>");
-  let buttonHeart = $("<div></div>");
+  let overlay = $('<div></div>');
+  let overlayButtonBox = $('<div></div>');
+  let buttonPlus = $('<div></div>');
+  let buttonHeart = $('<div></div>');
 
-  productTile.addClass("product-tile");
+  productTile.addClass('product-tile');
 
-  productTileImage.addClass("product-tile__image");
+  productTileImage.addClass('product-tile__image');
   productTileImage.html(productImage); // WARNING WITH THIS
 
-  productTileInfo.addClass("product-tile__info");
+  productTileInfo.addClass('product-tile__info');
   productTileInfo.append(productName);
   productTileInfo.append(productPrice);
 
-  buttonPlus.addClass("button-layer");
-  buttonPlus.attr("id", "btn-plus");
+  buttonPlus.addClass('button-layer');
+  buttonPlus.attr('id', 'btn-plus');
 
-  buttonHeart.addClass("button-layer");
-  buttonHeart.attr("id", "btn-heart");
+  buttonHeart.addClass('button-layer');
+  buttonHeart.attr('id', 'btn-heart');
 
   buttonPlus.html(`<img src="img/plus.png" alt="Plus icon"/>`);
   buttonHeart.html(`<img src="img/heart.png" alt="Heart icon"/>`);
 
-  overlayButtonBox.addClass("button-box");
+  overlayButtonBox.addClass('button-box');
   overlayButtonBox.append(buttonPlus);
   overlayButtonBox.append(buttonHeart);
-  overlay.addClass("overlay overlay--green");
+  overlay.addClass('overlay overlay--green');
   overlay.append(overlayButtonBox);
 
   productTile.append(productTileImage);
@@ -104,34 +102,34 @@ function createDefaultProductTile(product) {
  * @returns A node element to be append on the DOM
  */
 function createNoOverlayProductTile(product) {
-  let productTile = $("<div></div>");
+  let productTile = $('<div></div>');
 
-  let productTileImage = $("<div></div>");
-  let productTileInfo = $("<div></div>");
+  let productTileImage = $('<div></div>');
+  let productTileInfo = $('<div></div>');
 
-  let productImageLink = $("<a></a>");
+  let productImageLink = $('<a></a>');
   let productNameLink = $(`<a></a>`);
-  let priceBox = $("<div></div>");
-  let priceBoxSpan = $("<span></span>");
+  let priceBox = $('<div></div>');
+  let priceBoxSpan = $('<span></span>');
   let priceLink = $(`<a>$ ${product.price}</a>`);
   let buttonBuyNowLink = $(`<a>BUY NOW</a>`);
 
-  productImageLink.attr("href", "product-detailed-page.html");
+  productImageLink.attr('href', 'product-detailed-page.html');
 
   let productImage = $(`<img src=${product.imageUrl} />`);
-  productImage.attr("alt", product.name);
+  productImage.attr('alt', product.name);
 
   productImageLink.append(productImage);
 
-  productNameLink.attr("href", "product-detailed-page.html");
+  productNameLink.attr('href', 'product-detailed-page.html');
 
   let productName = $(`<p>${product.name}</p>`);
 
   productNameLink.append(productName);
 
-  priceLink.attr("href", "product-detailed-page.html");
-  priceLink.addClass("price");
-  buttonBuyNowLink.addClass("btn-buy");
+  priceLink.attr('href', 'product-detailed-page.html');
+  priceLink.addClass('price');
+  buttonBuyNowLink.addClass('btn-buy');
   buttonBuyNowLink.click(addToShoppingBag);
 
   productTileImage.append(productImageLink);
@@ -139,17 +137,17 @@ function createNoOverlayProductTile(product) {
   priceBoxSpan.append(priceLink);
   priceBoxSpan.append(buttonBuyNowLink);
 
-  priceBox.addClass("price-box");
+  priceBox.addClass('price-box');
   priceBox.append(priceBoxSpan);
 
   productTileInfo.append(productNameLink);
   productTileInfo.append(priceBox);
 
-  productTile.addClass("product-tile");
+  productTile.addClass('product-tile');
 
-  productTileImage.addClass("product-tile__image");
+  productTileImage.addClass('product-tile__image');
 
-  productTileInfo.addClass("product-tile__info");
+  productTileInfo.addClass('product-tile__info');
 
   productTile.append(productTileImage);
   productTile.append(productTileInfo);
@@ -164,31 +162,31 @@ function createNoOverlayProductTile(product) {
  * @returns A node element to be append on the DOM
  */
 function createStaticProductTile(product) {
-  let productTile = $("<div></div>");
+  let productTile = $('<div></div>');
   let productStaticImage = $(`<img src=${product.imageUrl} />`);
-  let overlay = $("<div></div>");
-  let overlayContent = $("<div></div>");
+  let overlay = $('<div></div>');
+  let overlayContent = $('<div></div>');
   let overlayText = $(
     `<p>My dragons are misbehaving again. Unbelieveable!</p>`
   );
-  let overlayHour = $("<div></div>");
-  let overlayHourIcon = $("<img />");
+  let overlayHour = $('<div></div>');
+  let overlayHourIcon = $('<img />');
   overlayHourIcon.attr({
-    src: "img/layer-img.png",
-    alt: "User and Speech Bubble Icons"
+    src: 'img/layer-img.png',
+    alt: 'User and Speech Bubble Icons'
   });
 
   let spanHour = $(`<span>5H AGO</span>`);
 
   productStaticImage.attr(
-    "alt",
-    "My dragons are misbehaving again. Unbelieveable!"
+    'alt',
+    'My dragons are misbehaving again. Unbelieveable!'
   );
 
-  productTile.addClass("product-tile product-tile--static");
-  overlay.addClass("overlay overlay--pink");
-  overlayContent.addClass("overlay--pink__content");
-  overlayHour.addClass("overlay--pink__hour");
+  productTile.addClass('product-tile product-tile--static');
+  overlay.addClass('overlay overlay--pink');
+  overlayContent.addClass('overlay--pink__content');
+  overlayHour.addClass('overlay--pink__hour');
 
   overlayHour.append(overlayHourIcon);
   overlayHour.append(spanHour);
@@ -223,3 +221,7 @@ function createGridRow(products) {
 export function shuffle(array) {
   array.sort(() => Math.random() - 0.5);
 }
+
+$buttonLoadMore.click(function() {
+  getData();
+});
