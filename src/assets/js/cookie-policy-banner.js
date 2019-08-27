396e1956f706c4bb3177bead4000777d211e7f88
Lucas Bertoldo <lucas.fber@gmail.com>
2019-08-27 13:32:54 -0300

    cookie banner scripts documented

--- src/assets/js/cookie-policy-banner.js
+++ src/assets/js/cookie-policy-banner.js
@@ -4,27 +4,43 @@ const $buttonAccept = $('.cookie-policy-content > button');
 const $cookiePolicyBanner = $('.cookie-policy-banner');
 const $buttonClosePolicyBanner = $('.button-close-banner');
 
+/**
+ * Tests if acceptCookiePolicy flag is not equals to true.
+ * If the result is true, then shows up the Cookie Policy Banner
+ */
 const showCookiePolicyBanner = () => {
   if (localStorage.getItem('acceptCookiePolicy') !== 'true') {
     $cookiePolicyBanner.fadeIn();
   }
 };
 
+/**
+ * Dispose the Cookie Policy Banner setting display to none
+ */
 const closeCookieBanner = () => {
   $cookiePolicyBanner.hide();
 };
 
-$buttonAccept.click(function() {
+/**
+ * Tests if the browser implements Local Storage funcionality.
+ * If true, then sets a item named acceptCookiePolicy to true, it will
+ * be the flag used to show or not the Cookie Policy Banner
+ */
+const handleAcceptCookiePolicy = () => {
   if (typeof Storage !== 'undefined') {
     localStorage.setItem('acceptCookiePolicy', true);
     closeCookieBanner();
   } else {
-    console.log('nao suportado');
+    alert('Local Storage feature is not supported by your browser.');
   }
-});
+};
 
 $buttonClosePolicyBanner.click(function() {
   closeCookieBanner();
 });
 
-setTimeout(showCookiePolicyBanner, 2000);
+$buttonAccept.click(function() {
+  handleAcceptCookiePolicy();
+});
+
+setTimeout(showCookiePolicyBanner, 10000);
